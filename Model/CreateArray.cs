fdc2877e10535a7533199069a475ff290aa410c7
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-11-06 09:17:51 -0300

    Criada classe Program

---
+++ Model/CreateArray.cs
@@ -0,0 +1,44 @@
+namespace ArraySort.Model;
+
+public class CreateArray
+{
+    public static int[] Ascending(int size)
+    {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = i;
+        }
+        return array;
+    }
+
+    public static int[] Random(int size)
+    {
+        Random rand = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = rand.Next();
+        }
+        return array;
+    }
+
+    public static int[] Descending(int size)
+    {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = size - i - 1;
+        }
+        return array;
+    }
+
+    public static void Print(int[] array)
+    {
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("");
+﻿using System.Diagnostics;
+using ArraySort.Algorithms;
+using ArraySort.Model;
+
+int[] sizes = { 1000, 10000, 100000 };
+object[] algorithms = { new BubbleSort(), new ImprovedBubbleSort() };
+
+foreach (object algorithm in algorithms)
+{
+    Console.WriteLine("\n--------------------------------------------------");
+    Console.WriteLine($"{algorithm.GetType().Name.ToUpper()}");
+    Console.WriteLine("--------------------------------------------------");
+    
+    foreach (int size in sizes)
+    {
+        int[] ascending = CreateArray.Ascending(size);
+        int[] random = CreateArray.Random(size);
+        int[] descending = CreateArray.Descending(size);
+        
+        Console.WriteLine($"************************\n{size} elementos\n************************");
+        
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        algorithm.GetType().GetMethod("Sort").Invoke(algorithm, new object[] { ascending });
+        stopwatch.Stop();
+        Console.WriteLine($"Ordenado: {stopwatch.Elapsed.ToString(@"m\:ss\.fff")}");
+
+        stopwatch.Reset();
+        stopwatch.Start();
+        algorithm.GetType().GetMethod("Sort").Invoke(algorithm, new object[] { random });
+        stopwatch.Stop();
+        Console.WriteLine($"Aleatório: {stopwatch.Elapsed.ToString(@"m\:ss\.fff")}");
+
+        stopwatch.Reset();
+        stopwatch.Start();
+        algorithm.GetType().GetMethod("Sort").Invoke(algorithm, new object[] { descending });
+        stopwatch.Stop();
+        Console.WriteLine($"Decrescente: {stopwatch.Elapsed.ToString(@"m\:ss\.fff")}\n");
+    }
+}
